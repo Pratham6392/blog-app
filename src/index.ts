2667720659f311688a3f0a03e015f6495f2b37af
Pratham khatri <khatripratham349@gmail.com>
2024-07-08 22:31:24 +0530

    backend doneb almosttt

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL: string
+  }
+}
+>()
+
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
 app.get('/', (c) => {
+  const prisma= new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
   return c.text('Hello Hono!')
 })
 
